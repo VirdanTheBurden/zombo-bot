9efd3025b28288b3fe75562042db102c956419d9
Luna <ojackson287@gmail.com>
2023-03-10 05:59:21 -0500

    fix: add constants.Bot.prefix to ZomboBot context manager

--- bot/__main__.py
+++ bot/__main__.py
@@ -15,7 +15,8 @@ log.setup()
 
 async def main():
     async with ZomboBot(
-        redis.Redis(host="localhost", port=6379), intents=nextcord.Intents.all()
+        redis.Redis(host="localhost", port=6379), intents=nextcord.Intents.all(),
+        command_prefix=constants.Bot.prefix
     ) as _bot:
         await _bot.start(constants.Bot.token)
 
