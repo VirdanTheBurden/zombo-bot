ef7b048432ab0adc6a41092f810a41c0775b5aae
Luna <ojackson287@gmail.com>
2023-03-10 20:31:02 -0500

    fix: added bot.__main__ logger instead of using root

--- bot/__main__.py
+++ bot/__main__.py
@@ -11,6 +11,7 @@ from bot import log
 
 
 log.setup()
+logger = logging.getLogger(__name__)
 
 
 async def main():
@@ -18,6 +19,7 @@ async def main():
         redis.Redis(host="localhost", port=6379), intents=nextcord.Intents.all(),
         command_prefix=constants.Bot.prefix
     ) as _bot:
+        logger.info("Bot is starting.")
         await _bot.start(constants.Bot.token)
 
 
