a2d6f29884e11758b0ddf8116966410803289ed3
Luna <ojackson287@gmail.com>
2023-03-10 20:28:39 -0500

    add: padding to logs

--- bot/log.py
+++ bot/log.py
@@ -2,13 +2,10 @@ import logging
 import coloredlogs
 from bot import constants
 
-
 def setup():
     """Setup loggers."""
 
-    format_str = (
-        "[%(levelname)s] [%(asctime)s] [%(name)s] | [%(funcName)s:%(lineno)d] | %(message)s"
-    )
+    format_str = "[%(asctime)s] [%(levelname)-8s] [%(name)-36s] | [%(funcName)20s:%(lineno)-5d] | %(message)s"
     date_fmt = "%Y-%m-%d %H:%M:%S"
 
     root_logger = logging.getLogger()
@@ -16,8 +13,9 @@ def setup():
     coloredlogs.install(
         level="DEBUG" if constants.debug else "INFO",
         logger=root_logger,
-        datefmt=date_fmt,
         fmt=format_str,
+        datefmt=date_fmt
+
     )
 
     logging.getLogger("nextcord").setLevel(logging.WARNING)
